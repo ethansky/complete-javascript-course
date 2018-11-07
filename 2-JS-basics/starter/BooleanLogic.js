3bff8f8ff7bd97e2462a13d42e5cbc8d8e673c8d
ethansky <fragglekids@gmail.com>
2018-11-07 12:58:53 -0600

    Javascript is pretty similar to Java syntax wise.

---
+++ 2-JS-basics/starter/BooleanLogic.js
@@ -0,0 +1,16 @@
+var firstname = 'John';
+var age = 16;
+
+if (age < 13) {
+    console.log(firstname,'is a boy');
+} else if (age > 20) {
+    console.log(firstname, 'is a man');
+} else {
+    console.log(firstname, 'is teen')
+}
+
+/*
+&& - and
+|| - or
+! - not
+ */
